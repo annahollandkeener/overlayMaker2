857e0defb53d01481fa88be509c11389c1defd14
Anna Keener <annahkeener@gmail.com>
2025-10-08 14:09:44 -0400

    has column test added

--- test.py
+++ test.py
@@ -11,9 +11,12 @@ def isPathValid(pathToTest):
     
 #check if the attribute table has a wl column
 def isWLColumn(blocksToTest):
+    #making input block a vector layer in order to access attribute table
     blocksVL = QgsVectorLayer(blocksToTest)
+    #getting index of 'wl'
     field_index = blocksVL.fieldNameIndex('wl')
 
+    #if the 'wl field doesn't exist, the index will be = -1  
     if field_index == -1:
         print(f"The field 'wl' does not exist in layer '{blocksToTest.name()}'.")
         print("---> Creating new 'wl' column based on DEM for water table generation.")
@@ -25,6 +28,23 @@ def isWLColumn(blocksToTest):
         print(f"---> Using existing 'wl' for water table generation.")
         
         return True
+    
+def hasColumn(blocksToTest, colName):
+    #making input block a vector layer in order to access attribute table
+    blocksVL = QgsVectorLayer(blocksToTest)
+    #getting index of 'wl'
+    field_index = blocksVL.fieldNameIndex(colName)
+
+    #if the field doesn't exist, the index will be = -1  
+    if field_index == -1:
+        raise KeyError((f"The field" + colName + " does not exist in layer '{blocksToTest.name()}'. Add a 'block' field in the blocks layer with block names. "))
+    
+
+
+
+
+
         
         
 
+#need to check that the blocks input has a block column 
